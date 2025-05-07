02f8ef5c22c47c791c36f4ff243985cea6c4dff6
Kirill Begej <fleki679@yandex.ru>
2025-05-07 15:45:58 +0300

    fix: исправить подключение шрифтов

--- app/fonts/fonts.ts
+++ app/fonts/fonts.ts
@@ -3,22 +3,22 @@ import localFont from "next/font/local";
 export const roboto = localFont({
   src: [
     {
-      path: "./roboto/roboto.woff2",
+      path: "./roboto/roboto-regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./roboto/roboto.woff",
+      path: "./roboto/roboto-regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./roboto/roboto.woff2",
+      path: "./roboto/roboto-bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./roboto/roboto.woff",
+      path: "./roboto/roboto-bold.woff",
       weight: "700",
       style: "normal",
     },
